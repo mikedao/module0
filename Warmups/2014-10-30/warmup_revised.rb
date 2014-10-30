0f7972b82366f9f2b9138ca25207f14bff36f7d5
Michael Dao <michael.dao@gmail.com>
2014-10-30 11:28:37 -0600

    Revised and refactored warmup exercise.

---
+++ Warmups/2014-10-30/warmup_revised.rb
@@ -0,0 +1,36 @@
+def sum_of_squares(limit)
+  array = []
+  (0..limit).each do |n|
+    array[n] = n
+  end
+
+  array.each do |n|
+   array[n] = array[n].to_i * array[n].to_i
+  end
+
+  sum = 0
+  array.each { |a| sum += a}
+  return sum
+end
+
+def sum_squared(limit)
+  array = []
+  sum = 0
+
+  (0..limit).each do |n|
+    array[n] = n
+  end
+
+    array.each { |a| sum += a }
+  return sum * sum
+end
+
+puts "The sum of the numbers from 1 to 10 squared is #{sum_squared(10)}"
+puts "The sum of the squares of 1 to 10 is #{sum_of_squares(10)}."
+puts "The difference between the two is #{sum_squared(10) - sum_of_squares(10)}"
+
+puts "\n"
+
+puts "The sum of the numbers from 1 to 100 squared is #{sum_squared(100)}"
+puts "The sum of the squares of 1 to 10 is #{sum_of_squares(100)}."
+puts "The difference between the two is #{sum_squared(100) - sum_of_squares(100)}"
